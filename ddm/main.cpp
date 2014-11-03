103558b30931952d2dc23a8e973b0224da2795ed
Zhao Yongfeng <zhaoyongfeng@ZhaodeMacBook-Pro-3.local>
2014-11-03 12:33:58 +0800

    Initial Commit

---
+++ ddm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ddm
+//
+//  Created by Zhao Yongfeng on 14/11/3.
+//  Copyright (c) 2014年 ZYF. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
